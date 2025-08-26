1ac6b9499085dde25ce423426656bf0b88bd1333
Jason Hensley <jason@secberus.com>
2025-08-26 10:08:30 -0400

    feat: implement the rest of the data types

--- model/model.go
+++ model/model.go
@@ -16,10 +16,70 @@ type (
 		Default *bool `json:"default,omitempty"`
 	}
 
+	Smallint struct {
+		Value   *int32 `json:"value,omitempty"`
+		Default *int32 `json:"default,omitempty"`
+	}
+
+	Bigint struct {
+		Value   *int64 `json:"value,omitempty"`
+		Default *int64 `json:"default,omitempty"`
+	}
+
+	Real struct {
+		Value   *float32 `json:"value,omitempty"`
+		Default *float32 `json:"default,omitempty"`
+	}
+
+	Double struct {
+		Value   *float64 `json:"value,omitempty"`
+		Default *float64 `json:"default,omitempty"`
+	}
+
+	Bytea struct {
+		Value   []byte `json:"value,omitempty"`
+		Default []byte `json:"default,omitempty"`
+	}
+
+	Timestamptz struct {
+		Value   *string `json:"value,omitempty"`
+		Default *string `json:"default,omitempty"`
+	}
+
+	Jsonb struct {
+		Value   *string `json:"value,omitempty"`
+		Default *string `json:"default,omitempty"`
+	}
+
+	Inet struct {
+		Value   *string `json:"value,omitempty"`
+		Default *string `json:"default,omitempty"`
+	}
+
+	Cidr struct {
+		Value   *string `json:"value,omitempty"`
+		Default *string `json:"default,omitempty"`
+	}
+
+	Macaddr struct {
+		Value   *string `json:"value,omitempty"`
+		Default *string `json:"default,omitempty"`
+	}
+
 	DataType struct {
-		Text    *Text    `json:"text,omitempty"`
-		Integer *Integer `json:"integer,omitempty"`
-		Boolean *Boolean `json:"boolean,omitempty"`
+		Text        *Text        `json:"text,omitempty"`
+		Integer     *Integer     `json:"integer,omitempty"`
+		Boolean     *Boolean     `json:"boolean,omitempty"`
+		Smallint    *Smallint    `json:"smallint,omitempty"`
+		Bigint      *Bigint      `json:"bigint,omitempty"`
+		Real        *Real        `json:"real,omitempty"`
+		Double      *Double      `json:"double,omitempty"`
+		Bytea       *Bytea       `json:"bytea,omitempty"`
+		Timestamptz *Timestamptz `json:"timestamptz,omitempty"`
+		Jsonb       *Jsonb       `json:"jsonb,omitempty"`
+		Inet        *Inet        `json:"inet,omitempty"`
+		Cidr        *Cidr        `json:"cidr,omitempty"`
+		Macaddr     *Macaddr     `json:"macaddr,omitempty"`
 	}
 
 	Table struct {
